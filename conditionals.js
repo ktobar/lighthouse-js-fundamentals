0036fa2e5b0df4a746ad3a8d1479f6daa60e92ee
Kelvin <tobar411@gmail.com>
2020-10-16 22:12:58 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -33,7 +33,8 @@ if (isCitizen && age > 18) {
 if (temperature < -40 || temperature > 40) {
   console.log("Maybe going outside isn't such a great idea…");
 }
-//if (!raining) {
+//
+if (!raining) {
   console.log("Leave your umbrella at home!");
 }
 
